f58cc6547c3a705d442dda5fa952cf9b15b51509
oszto <peter.osztodi@infinitelambda.com>
2025-08-24 14:19:24 +0100

    save

--- mini_data_warehouse/models/silver/fund_events_v/fund_events_v.sql
+++ mini_data_warehouse/models/silver/fund_events_v/fund_events_v.sql
@@ -1,11 +1,3 @@
 select
-  trim(fund_name)                               as fund_name,
-  try_to_date(transaction_date, 'DD/MM/YYYY')   as event_date,
-  upper(trim(transaction_type))                 as transaction_type,
-  try_to_number(transaction_amount)             as transaction_amount,
-  try_to_number(fund_size)                      as fund_size_latest,
-  try_to_number(transaction_index)              as transaction_index,
-  trim(sector)                                  as sector,
-  trim(country)                                 as country,
-  trim(region)                                  as region
+*
 from {{ ref('stg_fund_data') }}
